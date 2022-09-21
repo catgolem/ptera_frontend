01c848c6756d45fd43ccf4b471a2d7b86e99dd5f
ei <ray03j@hashira46.localdomain>
2022-09-21 17:31:00 +0900

    [add]パブリックページの追加

--- pages/public/index.tsx
+++ pages/public/index.tsx
@@ -1,54 +1,120 @@
-import React from 'react'
+import { NextPage } from 'next'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import PostItems from '../../components/PostItems'
 
-{/*
-ヘッダー
-    ユーザーネーム
-    ログアウトボタン
-    ログインボタン
-BG
-    横の背景
-センター
-    投稿  map関数
-    PostItemコンポーネントを入れる
 
-モーダル
-定義して  API
-*/}
+
 
 type postdata = {
-  username: string
-  text: string
+  number: number
+  thought: string
+  point: number
+  url: string
+  work: string
 }
 
-const User = () => {
+export const POSTS =[
+  {
+    number: 1,
+    thought: 'nice',
+    point: 100,
+    url: 'https://sample',
+    work: 'frozen',
+  },
+  {
+    number: 2,
+    thought: '感動した',
+    point: 80,
+    url: 'https://sample2',
+    work: 'ワンピース',
+  },
+  {
+    number: 3,
+    thought: 'hoge',
+    point: 46,
+    url: 'https://sample3',
+    work: 'fuga',
+  }
+
+]
+
+
+
+const Public:NextPage = () => {
+  
   return (
-    <div>
+    <Full>
       
       <BG>
+        
         <Center>
           
+          <PostItems />
+          
         </Center>
-        <ModalButton>
-
-        </ModalButton>
+        <ShareButton>
+          <Img src="/share_button.svg" />
+        </ShareButton>
       </BG>
-
-
-    </div>
+      
+    </Full>
   )
 }
 
-export default User
-
 const Center = styled.div`
-  
+  background-color:#f4c530;
+  width: 70vw; /* 好きな幅 */
+  max-width: 100%;
+  margin: 0 auto;
+  padding: 24px;
+  min-height: 100vh;
 `
 
 const BG = styled.div`
-  
+  background-color: #e73245;
+  min-height: 100vh;
+
 `
 
-const ModalButton = styled.div`
-  
+const ModalButton = styled.button`
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: white;
+  box-shadow: 0 5px #0023b7;
+  border-radius: 100px;
 `
+
+const ShareButton = styled.button`
+  position: fixed;
+  bottom: 80px;
+  right: 10px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: white;
+  box-shadow: 0 5px #0023b7;
+  border-radius: 100px;
+`
+
+const Full = styled.div`
+
+`
+
+const Img = styled.img`
+  position: absolute;
+  bottom: 3px;
+  right: 3px;
+  width: 45px;
+  height: 45px;
+
+
+`
+
+
+export default Public
+
